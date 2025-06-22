d5ec824e1df9c5c44c891d0f3b9aed2ae91a2540
Mahendra <mahendra5pute55@gmail.com>
2025-06-22 12:05:30 +0530

    first program

---
+++ src/test/java/Programs/SumArray.java
@@ -0,0 +1,12 @@
+package Programs;
+
+public class SumArray {
+    public static void main(String[] args) {
+        int[]a={1,2,3,4};
+        int sum=0;
+        for(int value:a){
+            sum=value+sum;
+        }
+        System.out.println(sum);
+    }
+}
